1dd1a27ecae8e7e366be12289f3e1937cd08a944
DimuHvH <info@dimu.dev>
2024-04-20 01:15:26 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Move User Certs (Android 14)
+
+Based on [this article](https://httptoolkit.com/blog/android-14-install-system-ca-certificate/).
+
+This Magisk module moves certificates from the user certificate store to the system store.
+
+## Why would I want user certificates in my system store?
+
+Mainly to intercept traffic using your own CA certs from i.e. Burp. 
+This module is mainly focused to fix the "burden" of injecting the certificates manually into zygote after each reboot of the device.
+
+## Important Notice
+
+This was solely created for Android 14, not higher not lower. If you need this functionality but for older versions of Android use [Move Certficates](https://github.com/Magisk-Modules-Repo/movecert).
+
+## Usage
+
+1. Download the `.zip` file from the [latest release][latestrelease].
+2. Go to *Magisk -> Modules -> Install from storage* and select the downloaded `.zip` file.
+3. Reboot.
+
+If a new version comes out (prob wont), repeat steps.
+
+The module does its work during the system boot. If you add new certificate(s),
+you'll have to reboot the device for the new certificate to be copied to the system store.
+
+## Building
+```shell
+./dist.sh
+```
---
+++ module/service.sh
@@ -0,0 +1,46 @@
+#!/system/bin/sh
+
+MODDIR=${0%/*}
+
+# wait for boot to complete
+while [ "$(getprop sys.boot_completed)" != 1 ]; do
+    sleep 1
+done
+
+# Create a separate temp directory, to hold the current certificates
+# Otherwise, when we add the mount we can't read the current certs anymore.
+mkdir -p -m 700 /data/local/tmp/tmp-ca-copy
+
+# Copy out the existing certificates
+cp /apex/com.android.conscrypt/cacerts/* /data/local/tmp/tmp-ca-copy/
+
+# Create the in-memory mount on top of the system certs folder
+mount -t tmpfs tmpfs /system/etc/security/cacerts
+
+# Copy the existing certs back into the tmpfs, so we keep trusting them
+mv /data/local/tmp/tmp-ca-copy/* /system/etc/security/cacerts/
+
+# Copy our new cert in, so we trust that too
+cp /data/misc/user/0/cacerts-added/* /system/etc/security/cacerts/
+
+# Update the perms & selinux context labels
+chown root:root /system/etc/security/cacerts/*
+chmod 644 /system/etc/security/cacerts/*
+chcon u:object_r:system_file:s0 /system/etc/security/cacerts/*
+
+# Deal with the APEX overrides, which need injecting into each namespace:
+# First we get the Zygote process(es), which launch each app
+ZYGOTE_PID=$(pidof zygote || true)
+ZYGOTE64_PID=$(pidof zygote64 || true)
+
+# Apps inherit the Zygote's mounts at startup, so we inject here to ensure
+# all newly started apps will see these certs straight away:
+for Z_PID in "$ZYGOTE_PID" "$ZYGOTE64_PID"; do
+    if [ -n "$Z_PID" ]; then
+        nsenter --mount=/proc/$Z_PID/ns/mnt -- \
+            /bin/mount --bind /system/etc/security/cacerts /apex/com.android.conscrypt/cacerts
+    fi
+done
+
+umount /system/etc/security/cacerts
+rmdir /system/etc/security/cacerts
